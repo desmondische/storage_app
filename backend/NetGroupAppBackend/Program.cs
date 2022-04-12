b853e18178efd28ebb8bb0c9ea55327f1bf34fab
Daniil Norin <danori@ttu.ee>
2022-04-12 19:20:37 +0300

    removed password restrictions

--- backend/NetGroupAppBackend/Program.cs
+++ backend/NetGroupAppBackend/Program.cs
@@ -58,6 +58,16 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<DataContext>()
     .AddDefaultTokenProviders();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
+
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
